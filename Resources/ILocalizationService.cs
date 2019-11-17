5087df0baa48a91120e3b188864bc0feb15070aa
Yunus Emre <yunusemre.kirkanahtar@cutesafe.com>
2019-11-17 23:35:23 +0300

    Init

---
+++ Resources/ILocalizationService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Localization;
+
+namespace CustomFramework.WebApiUtils.Contracts.Resources
+{
+    public interface ILocalizationService
+    {
+        string GetValue(string key);
+    }
+}
---
+++ WebApiResponse.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace CustomFramework.WebApiUtils.Contracts
+{
+    public class WebApiResponse
+    {
+        public HttpStatusCode StatusCode { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string Message { get; set; }
+
+        public object Result { get; set; }
+
+        public int TotalCount { get; set; }
+
+        public ErrorResponse ErrorResponse { get; set; }
+    }
+}
