7e2680966e4b9d893392cf43d8c65775494b8d60
Yunus Emre <yunusemre.kirkanahtar@cutesafe.com>
2019-11-26 14:36:02 +0300

    Hata mesaj şablonlarına Range değeri eklendi

--- ErrorMessages.cs
+++ ErrorMessages.cs
@@ -5,10 +5,10 @@ namespace CustomFramework.WebApiUtils.Contracts
         public const string Required = "The {0} field is required.";
         public const string StringLength = "The field {0} must be a string with a minimum length of {2} and a maximum length of {1}.";
         public const string RangeWithMinValue = "The field {0} must be greater than or equal to {1}.";
+        public const string RangeError = "The field {0} must be between {1} and {2}.";
         public const string EmailAddressNotValid = "The {0} field is not a valid e-mail address.";
         public const string Compare = "{0} and {1} do not match.";
         public const string InvalidDateOfBirth = "Invalid date of birth";
         public const string InvalidDate = "Invalid date";
-
     }
 }
